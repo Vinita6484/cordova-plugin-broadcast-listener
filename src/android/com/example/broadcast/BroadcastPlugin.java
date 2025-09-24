c83fb6d866ef6d9051cbc797e31ead71190f3c19
Vinita6484 <85390301+Vinita6484@users.noreply.github.com>
2025-09-24 13:22:55 +0530

    Create BroadcastPlugin.java

---
+++ src/android/com/example/broadcast/BroadcastPlugin.java
@@ -0,0 +1,34 @@
+package com.example.broadcast;
+
+import android.content.IntentFilter;
+import org.apache.cordova.*;
+import org.json.JSONArray;
+
+public class BroadcastPlugin extends CordovaPlugin {
+    private IntentListener receiver;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        if ("startListening".equals(action)) {
+            IntentListener.callbackContext = callbackContext;
+            if (receiver == null) {
+                receiver = new IntentListener();
+                IntentFilter filter = new IntentFilter("com.zebra.broadcast.SCAN"); // Zebra DataWedge action
+                cordova.getActivity().registerReceiver(receiver, filter);
+            }
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (receiver != null) {
+            cordova.getActivity().unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+}
