35081c693116522531fd85b19230d3d8f2243d2f
Vinita6484 <85390301+Vinita6484@users.noreply.github.com>
2025-09-24 15:28:57 +0530

    Update broadcast.js

--- www/broadcast.js
+++ www/broadcast.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.startListening = function(success, error) {
     exec(success, error, "BroadcastPlugin", "startListening", []);
 };
+
+exports.stopListening = function(success, error) {
+    exec(success, error, "BroadcastPlugin", "stopListening", []);
+};
